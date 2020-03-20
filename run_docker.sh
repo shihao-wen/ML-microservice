6e21476476ea2c3899e056abf8071105fd0661eb
Shihao Wen <wenshihao22@gmail.com>
2020-03-20 14:46:14 -0400

    update run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag=ml-micro .
 # Step 2: 
 # List docker images
-
+docker images ls
 # Step 3: 
 # Run flask app
+docker run -ti --rm --name ml-micro -p8080:8080 ml-micro

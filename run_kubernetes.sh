880cd22385b8a2622fc62220b6eeb8c00a7b0d6a
Shihao Wen <wenshihao22@gmail.com>
2020-03-20 16:01:35 -0400

    kub

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=wenshihao1993/ml-micro
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment ml-micro --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/ml-micro 8080
